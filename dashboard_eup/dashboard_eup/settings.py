41509b41e7c27be900ed8f563502d234258e02c7
Joao Basanta Alvarado <joao.basa.18@gmail.com>
2021-09-06 15:21:47 -0500

    Update settings.py

--- dashboard_eup/dashboard_eup/settings.py
+++ dashboard_eup/dashboard_eup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2peesx-^%z8stmf#4+$*iv%n)g(0jddvgx9xom@i=wr8ez0*jx'
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
