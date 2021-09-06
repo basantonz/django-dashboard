7d1dfc521591042af27f128b1140bb13f350f030
Joao Basanta Alvarado <joao.basa.18@gmail.com>
2021-09-06 14:24:49 -0500

    configuración inicial

--- dashboard_eup/dash/views.py
+++ dashboard_eup/dash/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+tablita = ['dato1\t','dato2\t','dato3\t','dato4\t','dato5\t']
+
+def home(request):
+	context = {'tablita':tablita}
+	return render (request , "dash/home.html", context) 
