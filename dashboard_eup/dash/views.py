5f08a8c178280e94904fccf9e9537fb8d560b6a9
Joao Basanta Alvarado <joao.basa.18@gmail.com>
2021-09-15 17:22:19 -0500

    Corrección de orden
    Co-Authored-By: terbas1 <39531891+Terbas1@users.noreply.github.com>

--- dashboard_eup/dash/views.py
+++ dashboard_eup/dash/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 import requests,json
+
+
+cadenaFashion='http://ec2-18-222-217-205.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
+cadenaKid='http://ec2-3-136-157-90.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
+cadenaPets='http://ec2-3-22-61-127.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
+cadenaHouseful=''
+cadenaElectro='http://ec2-3-139-57-54.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
+
+
+
 def ConvertirLista(cadena):
 	valores=[]
 	try:
@@ -14,22 +24,16 @@ def ConvertirLista(cadena):
 			valores.append('Vacío')
 	return valores
 
-cadenaFashion='http://ec2-18-222-217-205.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
-cadenaKid='http://ec2-3-136-157-90.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
-cadenaPets='http://ec2-3-22-61-127.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
-cadenaHouseful='http://–/api/v2.0/getStoreMetrics/'
-cadenaElectro='http://ec2-3-139-57-54.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
-
-listaFashion=ConvertirLista(cadenaFashion)
-listaKid=ConvertirLista(cadenaKid)
-listaPets=ConvertirLista(cadenaPets)
-listaHouseful=ConvertirLista(cadenaHouseful)
-listaElectro=ConvertirLista(cadenaElectro)
-
-tablita = ['datatable1','datatable2','datatable3']
-tablita2 = ['datatable4','datatable5']
-
 def home(request):
+	listaFashion=ConvertirLista(cadenaFashion)
+	print('lista fashion ok')
+	listaKid=ConvertirLista(cadenaKid)
+	listaPets=ConvertirLista(cadenaPets)
+	listaHouseful=ConvertirLista(cadenaHouseful)
+	listaElectro=ConvertirLista(cadenaElectro)
+
+	tablita = ['datatable1','datatable2','datatable3']
+	tablita2 = ['datatable4','datatable5']
 	context = { 'tablita':tablita,
 				'tablita2':tablita2,
 
