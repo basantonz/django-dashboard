fee6cb7a7bf5494571d0809b1b01d18ed000ee90
Joao Basanta Alvarado <joao.basa.18@gmail.com>
2021-09-27 12:17:24 -0500

    Cambio de paradigma

--- dashboard_eup/dash/controllers/dashControllers.py
+++ dashboard_eup/dash/controllers/dashControllers.py
@@ -4,6 +4,7 @@ import json
 from dash.models import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from dash.models import Fashion, Kid, Pets, Houseful, Electro
 
 
 @api_view(['GET'])
@@ -38,3 +39,49 @@ def ConvertirLista(cadena, type_of_cadena):
             for i in range(24):
                 valores.append(0)
     return valores
+
+def ConvertirLista2(tienda,cantidad):
+    arregglo=[]
+    arregglo_arreggladdo=[]
+    if tienda == 'fashion':
+        ultimos = Fashion.objects.all().order_by('-idFashion')[:cantidad]
+    elif tienda == 'kid':
+        ultimos = Kid.objects.all().order_by('-idKid')[:cantidad]
+    elif tienda == 'pets':
+        ultimos = Pets.objects.all().order_by('-idPets')[:cantidad]
+    elif tienda == 'houseful':
+        ultimos = Houseful.objects.all().order_by('-idHouseful')[:cantidad]
+    elif tienda == 'electro':
+        ultimos = Electro.objects.all().order_by('-idElectro')[:cantidad]
+    for x in reversed(ultimos):
+        arregglo.append(x.actualizar)
+        arregglo.append(x.sixpm_actualizar)
+        arregglo.append(x.amazon_actualizar)
+        arregglo.append(x.ebay_actualizar)
+        arregglo.append(x.footlocker_actualizar)
+        arregglo.append(x.nike_actualizar)
+        arregglo.append(x.zappos_actualizar)
+        arregglo.append(x.actualizados)
+        arregglo.append(x.sixpm_actualizados)
+        arregglo.append(x.amazon_actualizados)
+        arregglo.append(x.ebay_actualizados)
+        arregglo.append(x.footlocker_actualizados)
+        arregglo.append(x.nike_actualizados)
+        arregglo.append(x.zappos_actualizados)
+        arregglo.append(x.creados)
+        arregglo.append(x.sixpm_creados)
+        arregglo.append(x.amazon_creados)
+        arregglo.append(x.ebay_creados)
+        arregglo.append(x.footlocker_creados)
+        arregglo.append(x.nike_creados)
+        arregglo.append(x.zappos_creados)
+        arregglo.append(x.listos_subir_meli)
+        arregglo.append(x.listos_subir_zyte)
+        arregglo.append(x.all_products)
+    for x in arregglo:
+        if x is None:
+            arregglo_arreggladdo.append(0)
+        else:
+            arregglo_arreggladdo.append(x)
+    return arregglo_arreggladdo
+            
--- dashboard_eup/dash/views.py
+++ dashboard_eup/dash/views.py
@@ -1,28 +1,33 @@
 from django.shortcuts import render
 import requests
 import json
-from .controllers.dashControllers import ConvertirLista
+from .controllers.dashControllers import ConvertirLista, ConvertirLista2
 from dash.models import Fashion, Kid, Pets, Houseful, Electro
 
-cadenaFashion = 'http://ec2-18-222-217-205.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
-cadenaKid = 'http://ec2-3-136-157-90.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
-cadenaPets = 'http://ec2-3-22-61-127.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
-cadenaHouseful = ''
-cadenaElectro = 'http://ec2-3-139-57-54.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
-
-#cadenaFashion = ''
-#cadenaKid = ''
-#cadenaPets = ''
+#cadenaFashion = 'http://ec2-18-222-217-205.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
+#cadenaKid = 'http://ec2-3-136-157-90.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
+#cadenaPets = 'http://ec2-3-22-61-127.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
 #cadenaHouseful = ''
-#cadenaElectro = ''
+#cadenaElectro = 'http://ec2-3-139-57-54.us-east-2.compute.amazonaws.com/api/v2.0/getStoreMetrics/'
+
+cadenaFashion = ''
+cadenaKid = ''
+cadenaPets = ''
+cadenaHouseful = ''
+cadenaElectro = ''
 
 def home(request):
-    listaFashion = ConvertirLista(cadenaFashion, "web")
-    listaKid = ConvertirLista(cadenaKid, "web")
-    listaPets = ConvertirLista(cadenaPets, "web")
-    listaHouseful = ConvertirLista(cadenaHouseful, "web")
-    listaElectro = ConvertirLista(cadenaElectro, "web")
+    ultimos_sub_Fashion = Fashion.objects.all().order_by('-idFashion')[:1]
+    ultimos_sub_Kid = Kid.objects.all().order_by('-idKid')[:1]
+    ultimos_sub_Pets = Pets.objects.all().order_by('-idPets')[:1]
+    ultimos_sub_Houseful = Houseful.objects.all().order_by('-idHouseful')[:1]
+    ultimos_sub_Electro = Electro.objects.all().order_by('-idElectro')[:1]
 
+    listaFashion = ConvertirLista2("fashion", 1)
+    listaKid = ConvertirLista2("kid", 1)
+    listaPets = ConvertirLista2("pets", 1)
+    listaHouseful = ConvertirLista2("houseful", 1)
+    listaElectro = ConvertirLista2("electro", 1)
     tablita = ['datatable1', 'datatable2', 'datatable3']
     tablita2 = ['datatable4', 'datatable5']
     context = {'tablita': tablita,
@@ -37,83 +42,16 @@ def home(request):
     return render(request, "dash/home.html", context)
 
 def graficos(request):
-    listaFashion = ConvertirLista(cadenaFashion, "grafica")
-    listaKid = ConvertirLista(cadenaKid, "grafica")
-    listaPets = ConvertirLista(cadenaPets, "grafica")
-    listaHouseful = ConvertirLista(cadenaHouseful, "grafica")
-    listaElectro = ConvertirLista(cadenaElectro, "grafica")
-
-    ultimos_sub_Fashion = Fashion.objects.all().order_by('-idFashion')[:5]
-    ultimos_sub_Kid = Kid.objects.all().order_by('-idKid')[:5]
-    ultimos_sub_Pets = Pets.objects.all().order_by('-idPets')[:5]
-    ultimos_sub_Houseful = Houseful.objects.all().order_by('-idHouseful')[:5]
-    ultimos_sub_Electro = Electro.objects.all().order_by('-idElectro')[:5]
-    cont = 0
-    FashionArr=[]
-    KidArr=[]
-    PetsArr=[]
-    HousefulArr=[]
-    ElectroArr=[]
-
-    for x in reversed(ultimos_sub_Fashion):
-        print(x)
-        FashionArr.append(x.all_products)
-        cont=cont+1
-        if cont==5:
-            cont=0
-            break
-        else:
-            continue
+    FashionAllArr=ConvertirLista2("fashion",6)
+    KidAllArr=ConvertirLista2("kid",6)
+    PetsAllArr=ConvertirLista2("pets",6)
+    HousefulAllArr=ConvertirLista2("houseful",6)
+    ElectroAllArr=ConvertirLista2("electro",6)
     
-    for x in reversed(ultimos_sub_Kid):
-        print(x)
-        KidArr.append(x.all_products)
-        cont=cont+1
-        if cont==5:
-            cont=0
-            break
-        else:
-            continue
-    
-    for x in reversed(ultimos_sub_Pets):
-        print(x)
-        PetsArr.append(x.all_products)
-        cont=cont+1
-        if cont==5:
-            cont=0
-            break
-        else:
-            continue
-    
-    for x in reversed(ultimos_sub_Houseful):
-        print(x)
-        HousefulArr.append(x.all_products)
-        cont=cont+1
-        if cont==5:
-            cont=0
-            break
-        else:
-            continue
-    
-    for x in reversed(ultimos_sub_Electro):
-        print(x)
-        ElectroArr.append(x.all_products)
-        cont=cont+1
-        if cont==5:
-            cont=0
-            break
-        else:
-            continue
-    
-    context = {'listaFashion': listaFashion,
-               'listaKid': listaKid,
-               'listaPets': listaPets,
-               'listaHouseful': listaHouseful,
-               'listaElectro': listaElectro,
-               'FashionArr': FashionArr,
-                'KidArr': KidArr,
-                'PetsArr': PetsArr,
-                'HousefulArr': HousefulArr,
-                'ElectroArr': ElectroArr 
+    context = {'FashionAllArr': FashionAllArr,
+                'KidAllArr': KidAllArr,
+                'PetsAllArr': PetsAllArr,
+                'HousefulAllArr': HousefulAllArr,
+                'ElectroAllArr': ElectroAllArr 
                }
     return render(request, "dash/graficos.html",context)
